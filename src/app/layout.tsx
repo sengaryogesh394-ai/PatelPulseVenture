faedc84601790179350e9cd66fea9d4d0197d736
rohitsengarppv-gif <rohitsengarppv@gmail.com>
2025-11-15 14:55:27 +0530

    added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { cn } from '@/lib/utils';
@@ -25,6 +26,32 @@ export default function RootLayout({
         <link rel="icon" href="/logo.png"  />
       </head>
       <body suppressHydrationWarning className={cn('font-body antialiased', 'min-h-screen bg-background font-sans')}>
+        {/* Meta Pixel Code */}
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s){
+              if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)
+            }(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1165750848358240');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style={{ display: 'none' }} src="https://www.facebook.com/tr?id=1165750848358240&ev=PageView&noscript=1" />
+        </noscript>
+        {/* End Meta Pixel Code */}
+        {/* Ahrefs Analytics */}
+        <Script
+          src="https://analytics.ahrefs.com/analytics.js"
+          strategy="afterInteractive"
+          data-key="xV/xv78l0FT3sylNVSKtLQ"
+        />
+        {/* End Ahrefs Analytics */}
         <ThemeProvider
             attribute="class"
             defaultTheme="system"
